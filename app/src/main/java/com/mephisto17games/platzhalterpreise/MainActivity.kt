bfce4ebfd4767c06138d38b31a19b25b0f9d1ce5
OScheer <o.t.scheer@gmail,com>
2019-03-05 10:39:53 +0100

    Sound fix

--- app/src/main/java/com/mephisto17games/platzhalterpreise/MainActivity.kt
+++ app/src/main/java/com/mephisto17games/platzhalterpreise/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mephisto17games.platzhalterpreise
 
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.media.AudioManager
@@ -8,6 +9,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.transition.Scene
 import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.widget.*
 import android.widget.AdapterView
@@ -15,6 +17,10 @@ import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
+import android.content.Context.AUDIO_SERVICE
+import android.support.v4.content.ContextCompat.getSystemService
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,42 +33,35 @@ class MainActivity : AppCompatActivity() {
 
         val creditsButton = findViewById<Button>(R.id.creditsButton)
 
-        val checkMusic = findViewById<Switch>(R.id.music)
+        /*val howToPlayButton = findViewById<Button>(R.id.howToPlayButton)*/
 
         val player1Name = findViewById<EditText>(R.id.p1DefaultName)
 
         // Musik im Hintergrund
         // Credits: https://www.bensound.com
         // The Elevator Bossa Nova
-        //val mp = MediaPlayer.create(this, R.raw.bensound_elevator)
-        //val mp = MediaPlayer.create(this, R.raw.verklixx)
-        //mp.start()
         Music.play(this, R.raw.bensound_elevator)
 
-        // Speichern ob Musik Off aktiviert ist
+        // Abfragen ob Musik Off -> true
         val musicOff: SharedPreferences = getSharedPreferences("musicOff",0)
         val editor: SharedPreferences.Editor = musicOff.edit()
         music.isChecked = musicOff.getBoolean("musicOff", false)
 
+        // Zugriff auf Audioplayer
         val audioManager: AudioManager = getSystemService(AUDIO_SERVICE) as AudioManager
-        val mediaCurrentVolume: Int = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+        // Audio auf 50% schalten
+        val media50PercentVolume: Int = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)*100/50
+        // Audio stumm schalten
         val mediaMinVolume: Int = audioManager.getStreamMinVolume(AudioManager.STREAM_MUSIC)
 
-/*
-        val musicOn: SharedPreferences = getSharedPreferences("musicCurrentVolume", 1)
-        val editorMusicOn: SharedPreferences.Editor = musicOn.edit()
-        val CurrentVolume = musicOn.getInt("musicCurrentVolume", mediaCurrentVolume)
-        editorMusicOn.putInt("musicCurrentVolume", mediaCurrentVolume)
-        editorMusicOn.commit()
-*/
-
+        // Aktivierung des Switch und speichern in Prefs
         music.setOnClickListener(View.OnClickListener {
             if(music.isChecked){
                 Music.stop(this)
                 audioManager.setStreamVolume(
                     AudioManager.STREAM_MUSIC, // Stream type
                     mediaMinVolume, // Volume index
-                    AudioManager.FLAG_SHOW_UI// Flags
+                    AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE// Flags
                 )
                 editor.putBoolean("musicOff", true)
                 editor.commit()
@@ -70,8 +69,8 @@ class MainActivity : AppCompatActivity() {
                 Music.play(this, R.raw.bensound_elevator)
                 audioManager.setStreamVolume(
                     AudioManager.STREAM_MUSIC, // Stream type
-                    mediaCurrentVolume, // Volume index
-                    AudioManager.FLAG_SHOW_UI// Flags
+                    media50PercentVolume, // Volume index
+                    AudioManager.FLAG_PLAY_SOUND// Flags
                 )
                 editor.putBoolean("musicOff", false)
                 editor.commit()
@@ -103,10 +102,23 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
         })
 
+        // Click on Credits
         creditsButton.setOnClickListener(View.OnClickListener {
             val myIntent = Intent(this@MainActivity, CreditActivity::class.java)
             startActivity(myIntent)
         })
+
+        // Click on How to play?
+        /*howToPlayButton.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "1. Choose your player name and select the difficulty by choosing an easy (Schmal-Lars), " +
+                    "normal (Lars), or hard (Breit-Lars) opponent. \n" +
+                    "2. Try to guess the \"original\" price as close as possible.\n" +
+                    "3. Your bet was closer than the CPU's? Bravo, you earned 1 point. Guess the price correctly to earn the price in " +
+                    "points (up to 1000)! \n" +
+                    "4. You and Lars take turns guessing first. \n" +
+                    "5. Collect more points than your opponent in a game of 5 rounds, or extend to a 15 round marathon.",
+                Toast.LENGTH_LONG).show()
+        })*/
 
     }
 
